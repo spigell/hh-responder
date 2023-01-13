425adbd4d6f9c6654a24cf7b1dfee7da949e035b
Spigell <spigelly@gmail.com>
2023-01-14 03:41:36 +0400

    beautify version output

--- cmd/version.go
+++ cmd/version.go
@@ -15,7 +15,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
+		fmt.Printf("%s version: %s\n", app, version)
 	},
 }
 
